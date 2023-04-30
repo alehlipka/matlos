3aed8d99370db7ae5a4d62cd18663c6819832c24
Newfag <alehlipka@gmail.com>
2023-04-30 18:17:23 +0300

    Prices images

--- resources/js/Pages/Home/Price.vue
+++ resources/js/Pages/Home/Price.vue
@@ -11,67 +11,6 @@ import price3 from "@/../img/price/price_3.png";
                 <img :src="price1">
                 <img style="margin: 0 20px;" :src="price2">
                 <img :src="price3">
-                <!-- <div class="price-block">
-                    <div class="price-title">Dla dwojga</div>
-                    <div class="price-description">
-                        Przy zakupie otrzymasz:<br>
-                        2 pełne indywidualne odczyty Matrycy Losu ze wszystkimi znaczeniami każdego arkanu<br>
-                        interpretacja seksualności według twoich energii<br>
-                        świadomość duchowa<br>
-                        według twoich arkanów i energii
-                    </div>
-                    <div class="price-brutto">59,99 zł brutto</div>
-                    <div class="price-rabat">Po rabacie:</div>
-                    <div class="price">
-                        <div class="price-value">29.99</div>
-                        <div class="price-currency">
-                            <div class="price-currency-value">zł</div>
-                            <div class="price-currency-brutto">brutto</div>
-                        </div>
-                    </div>
-                    <div class="price-buy"></div>
-                </div> -->
-
-                <!-- <div class="price-block top">
-                    <div class="price-title">Wszystko dla siebie</div>
-                    <div class="price-description">
-                        Przy zakupie otrzymasz:<br>
-                        spersonalizowaną analizę twojej Matrycy Losu ze wszystkimi interpretacjami twoich
-                        arkanów<br>
-                        indywidualna analiza karty zdrowia „Czakr”<br>
-                        przewodnik po latach twojego życia od 18 do 80 lat
-                    </div>
-                    <div class="price-brutto">39,99 zł brutto</div>
-                    <div class="price-rabat">Po rabacie:</div>
-                    <div class="price">
-                        <div class="price-value">19.99</div>
-                        <div class="price-currency">
-                            <div class="price-currency-value">zł</div>
-                            <div class="price-currency-brutto">brutto</div>
-                        </div>
-                    </div>
-                    <div class="price-buy"></div>
-                </div> -->
-
-                <!-- <div class="price-block">
-                    <div class="price-title">Dla rodziny</div>
-                    <div class="price-description">
-                        Przy zakupie otrzymasz:<br>
-                        4 spersonalizowane odczyty Matrycy Losu z pełną interpretacja twoich arkanów<br>
-                        analiza programów rodowych<br>
-                        dostęp do konta użytkownika z historią zamówień<br>
-                    </div>
-                    <div class="price-brutto">99,99 zł brutto</div>
-                    <div class="price-rabat">Po rabacie:</div>
-                    <div class="price">
-                        <div class="price-value">49.99</div>
-                        <div class="price-currency">
-                            <div class="price-currency-value">zł</div>
-                            <div class="price-currency-brutto">brutto</div>
-                        </div>
-                    </div>
-                    <div class="price-buy"></div>
-                </div> -->
             </div>
         </div>
     </div>
@@ -84,43 +23,4 @@ import price3 from "@/../img/price/price_3.png";
     align-items: baseline;
     justify-content: center;
 }
-
-.price-block {
-    margin: 10px 10px 50px 10px;
-    background-color: #E5E5E5;
-    border-radius: 30px;
-    padding: 20px;
-    font-weight: bold;
-    color: #ffffff;
-    width: 220px;
-}
-
-.price-block.top {
-    background: linear-gradient(to right, orange 40%, red);
-}
-
-.price-block .price-title {
-    font-size: 24px;
-}
-
-.price-block .price-description {}
-
-.price-block .price-brutto {
-    font-size: 14px;
-    text-align: right;
-}
-
-.price-block .price-rabat {
-    font-size: 45px;
-}
-
-.price-block .price .price-value {}
-
-.price-block .price .price-currency {}
-
-.price-block .price-currency .price-currency-value {}
-
-.price-block .price-currency .price-currency-brutto {}
-
-.price-block .price-buy {}
 </style>
