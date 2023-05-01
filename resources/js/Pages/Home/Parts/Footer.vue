a9118d67b4e0282dadeda901f286db19e9c9903a
Newfag <alehlipka@gmail.com>
2023-05-01 13:02:03 +0300

    footer

--- resources/js/Pages/Home/Parts/Footer.vue
+++ resources/js/Pages/Home/Parts/Footer.vue
@@ -1,24 +1,54 @@
 <script setup>
-import iconUrl from "@/../img/footer/logo-small.png";
+import smallIconUrl from "@/../img/footer/logo-small.png";
+import backgroundUrl from "@/../img/footer/background.png";
 </script>
 
 <template>
-    <div id="footer" class="footer">
+    <div id="footer" :style="{ 'background-image': `url(${backgroundUrl})` }">
+        <div class="footer">
+            <div class="footainer">asd</div>
+            <div class="footainer">dsa</div>
+        </div>
+        <p class="copyright">
+            <img :src="smallIconUrl">
+            2023
+            <a href="#footer">matrycalosu.pl</a>
+        </p>
     </div>
-    <p class="copyright">
-        <img :src="iconUrl">
-        2023
-        <a href="#footer">matrycalosu.pl</a>
-    </p>
 </template>
 
 <style scoped>
+#footer {
+    background-repeat: no-repeat;
+    background-position-x: center;
+    background-position-y: bottom;
+    background-size: contain;
+    min-height: 320px;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+}
+
+.footer {
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+    margin: 0 90px 50px 90px;
+}
+
+.footainer {
+    /* width: 100%; */
+}
+
 .copyright {
     font-weight: bold;
-    margin-bottom: 20px;
     display: flex;
     align-items: center;
     justify-content: center;
+    position: absolute;
+    bottom: 15px;
+    width: 100%;
 }
 
 .copyright img {
