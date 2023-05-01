79751951bce7ad929f09eb91264d048d6d423126
Newfag <alehlipka@gmail.com>
2023-05-01 12:31:38 +0300

    footer copyright

--- resources/js/Pages/Home/Parts/Footer.vue
+++ resources/js/Pages/Home/Parts/Footer.vue
@@ -1,8 +1,33 @@
+<script setup>
+import iconUrl from "@/../img/footer/logo-small.png";
+</script>
+
 <template>
-    <div class="footer">
+    <div id="footer" class="footer">
     </div>
+    <p class="copyright">
+        <img :src="iconUrl">
+        2023
+        <a href="#footer">matrycalosu.pl</a>
+    </p>
 </template>
 
 <style scoped>
+.copyright {
+    font-weight: bold;
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
 
+.copyright img {
+    margin-right: 10px;
+}
+
+.copyright a {
+    text-decoration: none;
+    color: #000000;
+    margin-left: 10px;
+}
 </style>
