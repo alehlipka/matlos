fcce80f92b85134c25eaac5ac4841379fa2021bb
Newfag <alehlipka@gmail.com>
2023-05-01 10:42:33 +0300

    social

--- resources/js/Pages/Home/Parts/Social.vue
+++ resources/js/Pages/Home/Parts/Social.vue
@@ -1,5 +1,7 @@
 <script setup>
 import backgroundUrl from "@/../img/social/main.png";
+import instagramUrl from "@/../img/social/instagram.png";
+import tiktokUrl from "@/../img/social/tiktok.png";
 </script>
 
 <template>
@@ -7,10 +9,23 @@ import backgroundUrl from "@/../img/social/main.png";
         <div class="page-box">
             <div class="box-description" id="social-background" :style="{ 'background-image': `url(${backgroundUrl})` }">
                 <div class="inside">
-                    <div class="inside-title">Obserwuj nas w social media<br>żeby być na bieżąco z naszymi promocjami i nowościami</div>
-                    <div>
-                        <div></div>
-                        <div></div>
+                    <div class="inside-title">Obserwuj nas w social media<br>żeby być na bieżąco z naszymi promocjami i
+                        nowościami</div>
+                    <div class="social-container">
+                        <div>
+                            <div class="subscribe-title">Subskrybuj nas:</div>
+                            <div class="social-buttons">
+                                <a class="contact button social" href="#">
+                                    <img :src="tiktokUrl">
+                                    Nasz<br>TikTok</a>
+                                <a class="contact button social" href="#">
+                                    <img :src="instagramUrl">
+                                    Nasz<br>Instagram</a>
+                            </div>
+                        </div>
+                        <div>
+                            <a class="contact button b2o" href="#">Skontaktuj sie z nami</a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -30,16 +45,48 @@ import backgroundUrl from "@/../img/social/main.png";
     justify-content: flex-end;
 }
 
-.inside {
-    border: 2px dotted salmon;
+.social-buttons {
+    display: flex;
+}
 
-    height: 50%;
+.inside {
+    height: 40%;
     width: 1000px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 }
 
 .inside-title {
     font-weight: bold;
     font-size: 32px;
+}
+
+.social-container {
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+}
+
+.subscribe-title {
+    font-weight: bold;
+    margin-bottom: 16px;
+}
+
+.contact {
+    font-size: 16px;
+    padding: 8px 20px;
+    font-weight: bold;
+    display: flex;
+    align-items: center;
+}
+
+.contact.social {
+    padding: 8px 20px 8px 10px;
+    margin-right: 20px;
+}
+.contact.social img {
+    margin-right: 10px;
 }
 
 .page {
@@ -51,7 +98,5 @@ import backgroundUrl from "@/../img/social/main.png";
     width: 100%;
 }
 
-.box-description {
-    border: 2px dotted green;
-}
+.box-description {}
 </style>
